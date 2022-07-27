4b8862be8be18d261c1a3c5ae9aff00c595005a5
David Frantz <david.frantz@uni-trier.de>
2022-07-27 14:59:50 +0200

    add

---
+++ rstats/regression/linear-regression.r
@@ -0,0 +1,48 @@
+# this script runs the linear regression to analyze the relationship between building and infrastructure stocks
+# (C) Thomas Udelhoven, David Frantz
+
+require(dplyr)
+
+df_area_ <- read.csv("csv/joined/data_per-area.csv")
+
+
+year <- 2018
+
+data <- df_area_ %>%
+  filter(YEAR == year) %>%
+  select(
+    STATE_NAME,
+    mass_building,
+    mass_mobility
+  )
+
+names(data)
+
+
+# only one record "District of Columbia"
+z <- which(data$STATE_NAME=="District of Columbia")
+data <- data[-z,]
+
+
+data <- data[complete.cases(data), ]
+
+### Data analysis
+pairs(data %>% select(-STATE_NAME))
+
+
+# 50% samples from each STATE
+
+data_train <- data %>% group_by(STATE_NAME) %>% sample_frac(.5)
+data_valid <- data %>%  anti_join(data_train)
+
+# LM
+
+lm_data <-  lm(mass_building ~ mass_mobility -STATE_NAME, data = data_train)
+
+summary(lm_data)
+
+data_valid <- data_valid %>% 
+  mutate(predicted = predict(lm_data, data_valid)) %>%
+  mutate(observed = mass_building)
+
+cor(data_valid$observed, data_valid$predicted)^2
